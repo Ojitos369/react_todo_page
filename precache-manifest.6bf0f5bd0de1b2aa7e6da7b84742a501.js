049e79e25d49f9563ba16ea7c43afc6bf58e07c5
ojitos369 <ojitos369@gmail.com>
2022-02-18 15:09:04 -0600

    arrego en index

---
+++ precache-manifest.6bf0f5bd0de1b2aa7e6da7b84742a501.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0b2c4d6dd0e80652360b99bd3297084",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c4e5a6667452f211f322",
+    "url": "./static/css/main.3f808b56.chunk.css"
+  },
+  {
+    "revision": "f29d464af622d8fe9345",
+    "url": "./static/js/2.7c5e7d3b.chunk.js"
+  },
+  {
+    "revision": "c4e5a6667452f211f322",
+    "url": "./static/js/main.2d2df84d.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
